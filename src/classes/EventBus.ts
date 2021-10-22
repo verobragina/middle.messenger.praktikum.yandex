2b7bf6b5c78dd4b149809a2e2666ba44daa42c42
verobragina <verobragina@ya.ru>
2021-10-22 14:38:59 +0300

    Add classes - EventBus and Block

---
+++ src/classes/EventBus.ts
@@ -0,0 +1,36 @@
+export default class EventBus {
+    listeners: {
+        [key: string]: Function[]
+    }
+
+    constructor() {
+        this.listeners = {}
+    }
+
+    on(event: string, callback: Function) {
+        if (!this.listeners[event]) {
+            this.listeners[event] = []
+        }
+        this.listeners[event].push(callback)
+    }
+
+    emit(event: string, ...args: any) {
+        if (!this.listeners[event]) {
+            throw new Error(`Нет события: ${event}`)
+        }
+
+        this.listeners[event].forEach(function (listener) {
+            listener(...args)
+        })
+    }
+
+    off(event: string, callback: Function) {
+        if (!this.listeners[event]) {
+            throw new Error(`Нет события: ${event}`)
+        }
+
+        this.listeners[event] = this.listeners[event].filter(
+            listener => listener !== callback
+        )
+    }
+}
